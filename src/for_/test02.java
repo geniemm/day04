9539ff8243b9c58c7b5ca9c778081eebf010900f
myung <mong_jini@naver.com>
2023-06-16 08:50:17 +0900

    day04 수업내용  업로드

---
+++ src/for_/test02.java
@@ -0,0 +1,18 @@
+package for_;
+
+import java.util.Scanner;
+
+public class test02 {
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	int num;
+	System.out.print("수 입력: ");
+	num=sc.nextInt();
+	
+	for(int i=0;i<num;i++) {
+		for(int )
+	}
+}
+}
